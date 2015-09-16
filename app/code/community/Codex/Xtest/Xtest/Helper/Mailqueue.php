ce75285e951486cb8261bcc90f612da7e00a36e7
Matthias Kleine <matthias@klein0r.de>
2015-09-16 16:38:58 +0200

    check for isValidForSend
    Former-commit-id: 501dd88405c69da2d460c8083210d0c0db5267d0

--- app/code/community/Codex/Xtest/Xtest/Helper/Mailqueue.php
+++ app/code/community/Codex/Xtest/Xtest/Helper/Mailqueue.php
@@ -22,37 +22,37 @@ class Codex_Xtest_Xtest_Helper_Mailqueue
 
     public function addMail( Mage_Core_Model_Email_Template $mailObject, $email, $name, $variables )
     {
-
-        $emails = array_values((array)$email);
-        $names = is_array($name) ? $name : (array)$name;
-        $names = array_values($names);
-        foreach ($emails as $key => $email) {
-            if (!isset($names[$key])) {
-                $names[$key] = substr($email, 0, strpos($email, '@'));
+        if ($mailObject->isValidForSend()) {
+            $emails = array_values((array)$email);
+            $names = is_array($name) ? $name : (array)$name;
+            $names = array_values($names);
+            foreach ($emails as $key => $email) {
+                if (!isset($names[$key])) {
+                    $names[$key] = substr($email, 0, strpos($email, '@'));
+                }
             }
+
+            $variables['email'] = reset($emails);
+            $variables['name'] = reset($names);
+
+            $mailObject->setUseAbsoluteLinks(true);
+            $text = $mailObject->getProcessedTemplate($variables, true);
+
+            if( empty($email) ) {
+                throw Mage::exception('Codex_Xtest', 'to is empty', Codex_Xtest_Exception::EMPTY_MAIL_RECIPIENT);
+            }
+
+            if( empty($text) ) {
+                throw Mage::exception('Codex_Xtest', 'body is empty', Codex_Xtest_Exception::EMPTY_MAIL_BODY);
+            }
+
+            self::$_mailqueue[] = array(
+                'variables' => $variables,
+                'emails' => $emails,
+                'body' => $text,
+                'object' => $mailObject
+            );
         }
-
-        $variables['email'] = reset($emails);
-        $variables['name'] = reset($names);
-
-        $mailObject->setUseAbsoluteLinks(true);
-        $text = $mailObject->getProcessedTemplate($variables, true);
-
-        if( empty($email) ) {
-            throw Mage::exception('Codex_Xtest', 'to is empty', Codex_Xtest_Exception::EMPTY_MAIL_RECIPIENT);
-        }
-
-        if( empty($text) ) {
-            throw Mage::exception('Codex_Xtest', 'body is empty', Codex_Xtest_Exception::EMPTY_MAIL_BODY);
-        }
-
-        self::$_mailqueue[] = array(
-            'variables' => $variables,
-            'emails' => $emails,
-            'body' => $text,
-            'object' => $mailObject
-        );
-
         return $this;
     }
 
